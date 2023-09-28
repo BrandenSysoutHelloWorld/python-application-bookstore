17431af0364de1ab500d3890a21fedd5855bdae7
Branden <brandenconnected@gmail.com>
2023-09-28 21:52:39 +0200

    Addressed Issue: Automated pip Check & Install

--- START-BOOKSTORE.bat
+++ START-BOOKSTORE.bat
@@ -1,5 +1,23 @@
 @echo off
 
+rem Check if pip is installed
+where pip >nul 2>nul
+if errorlevel 1 (
+    echo Pip is not installed. Installing pip...
+    rem Robust approach when install pip automatically
+    python -m ensurepip --default-pip
+    rem Handle the unlikely event that the Batch file fails.
+    if errorlevel 1 (
+        echo Failed to install pip. Please install it manually.
+        pause
+        exit /b 1
+    ) else (
+        echo Pip has been successfully installed.
+    )
+) else (
+    echo Pip is already installed.
+)
+
 rem Get the file path of batch file
 set "script_dir=%~dp0"
 
